b30141a31132a842dbde1ccca69710dc71cd4676
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-07 08:08:06 -0700

    loading removed

--- app/index.tsx
+++ app/index.tsx
@@ -2,19 +2,16 @@ import { View, TextInput, Button, Text, StyleSheet } from "react-native";
 import { useState } from "react";
 
 export default function Home() {
-  const [formData, setFormData] = useState({ name: '', email: '' });
+  const [formData, setFormData] = useState({ name: "", email: "" });
   const [apiResponse, setApiResponse] = useState<any | null>(null);
-  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async () =>{
+  const handleSubmit = async () => {
     try {
-      setLoading(true);
       // const response = await fetch('');
       // const data = await response.json();
-      await new Promise((resolve) => setTimeout(resolve, 1000)); 
-      const resp = { message: 'User was successfully created' };
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const resp = { message: "User was successfully created" };
       setApiResponse(resp.message);
-      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -36,7 +33,7 @@ export default function Home() {
         onChangeText={(text) => setFormData({ ...formData, email: text })}
       />
       <Button title="Submit" onPress={handleSubmit} />
-      {loading && <Text>Loading...</Text>}
+
       {apiResponse && (
         <View style={styles.responseContainer}>
           <Text>Response:</Text>
@@ -45,22 +42,22 @@ export default function Home() {
       )}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 20,
   },
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 24,
     marginBottom: 20,
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     padding: 10,
     marginVertical: 10,
   },
